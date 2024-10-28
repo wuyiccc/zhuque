3e0503a301b42358bd4b70c45eaad887ee1ff5ff
wuyiccc <3406324191@qq.com>
2024-10-29 00:04:22 +0800

    feat: swiper

--- src/pages/demo/demo.tsx
+++ src/pages/demo/demo.tsx
@@ -1,6 +1,9 @@
-import {Button, Image, ScrollView, Text, View} from '@tarojs/components'
+import {Button, Image, ScrollView, Swiper, SwiperItem, Text, View} from '@tarojs/components'
 import CX1IMG from '@/asset/image/cx1.jpg'
 import CX2IMG from '@/asset/image/cx2.jpg'
+import CX3IMG from '@/asset/image/cx3.jpg'
+import CX4IMG from '@/asset/image/cx4.jpg'
+import CX5IMG from '@/asset/image/cx5.jpg'
 import './demo.less'
 
 function Demo() {
@@ -15,7 +18,8 @@ function Demo() {
       <Image className='image' src={CX1IMG}></Image>
       <Image className='image' src={CX2IMG}></Image>
       {/*网络图片*/}
-      <Image className='image' src='https://pss.bdstatic.com/static/superman/img/topnav/newfanyi-da0cea8f7e.png'></Image>
+      <Image className='image'
+             src='https://pss.bdstatic.com/static/superman/img/topnav/newfanyi-da0cea8f7e.png'></Image>
 
       {/*scrollView*/}
       {/*垂直滚动*/}
@@ -46,6 +50,14 @@ function Demo() {
         <View className='scrollItemX'>栏目1</View>
         <View className='scrollItemX'>栏目1</View>
       </ScrollView>
+
+      <Swiper indicatorDots className='swiperContainer'>
+        <SwiperItem><Image className='bannerItem' src={CX1IMG}></Image></SwiperItem>
+        <SwiperItem><Image className='bannerItem' src={CX2IMG}></Image></SwiperItem>
+        <SwiperItem><Image className='bannerItem' src={CX3IMG}></Image></SwiperItem>
+        <SwiperItem><Image className='bannerItem' src={CX4IMG}></Image></SwiperItem>
+        <SwiperItem><Image className='bannerItem' src={CX5IMG}></Image></SwiperItem>
+      </Swiper>
     </View>
   )
 }
