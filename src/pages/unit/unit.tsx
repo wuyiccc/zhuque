b9e2721689d5ee383ee99eec0170f680f1a71844
wuyiccc <3406324191@qq.com>
2024-10-30 00:01:53 +0800

    feat: 尺寸转换

--- src/pages/unit/unit.tsx
+++ src/pages/unit/unit.tsx
@@ -1,10 +1,27 @@
+import Taro from "@tarojs/taro";
 import {Text, View} from '@tarojs/components'
 import styles from './unit.module.less'
 
 function Unit() {
+
+
+  const lineStyle = {
+    // 转为小程序单位还是px, 单身等价于 rpx, 值会变
+    fontSize: Taro.pxTransform(100),
+    color: 'green'
+  }
+
   return (
     <View className={styles.container}>
-      <Text>Hello world! </Text>
+      <View>
+        <Text className={styles.unitTrans}>单位转换</Text>
+      </View>
+      <View>
+        <Text className={styles.unitNoTrans}>单位不转换</Text>
+      </View>
+      <View>
+        <Text style={lineStyle}>行内样式</Text>
+      </View>
     </View>
   )
 }
