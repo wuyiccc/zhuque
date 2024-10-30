1b558f29276832146060757e2bc70b8e0ac4ce32
wuyiccc <3406324191@qq.com>
2024-10-30 20:06:26 +0800

    feat: tabBar初始化

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,8 +1,10 @@
 export default defineAppConfig({
   pages: [
-    'pages/unit/unit',
-    'pages/demo/demo',
-    'pages/index/index',
+    'pages/home/index',
+    'pages/cart/index',
+    'pages/category/index',
+    'pages/profile/index',
+
   ],
   window: {
     // 下拉刷新时候的样式
@@ -13,9 +15,34 @@ export default defineAppConfig({
     // 导航栏文字颜色
     navigationBarTextStyle: 'white',
   },
-  permission: {
-    'scope.userLocation': {
-      desc: '你的位置信息将用于小程序位置接口的效果展示',
-    },
-  },
+  tabBar: {
+    color: '#999',
+    selectedColor: '#ff464e',
+    list: [
+      {
+        text: '首页',
+        pagePath: 'pages/home/index',
+        iconPath: './asset/image/tabbar/home.png',
+        selectedIconPath: './asset/image/tabbar/homeSelect.png'
+      },
+      {
+        text: '分类',
+        pagePath: 'pages/category/index',
+        iconPath: './asset/image/tabbar/category.png',
+        selectedIconPath: './asset/image/tabbar/categorySelect.png'
+      },
+      {
+        text: '购物车',
+        pagePath: 'pages/cart/index',
+        iconPath: './asset/image/tabbar/cart.png',
+        selectedIconPath: './asset/image/tabbar/cartSelect.png'
+      },
+      {
+        text: '我的',
+        pagePath: 'pages/profile/index',
+        iconPath: './asset/image/tabbar/profile.png',
+        selectedIconPath: './asset/image/tabbar/profileSelect.png'
+      }
+    ]
+  }
 })
