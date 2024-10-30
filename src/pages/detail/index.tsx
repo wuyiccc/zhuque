7536decabf868571c90fcafefaf5d2235e267322
wuyiccc <3406324191@qq.com>
2024-10-30 23:39:50 +0800

    feat: 微信小程序事件发送机制

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,11 +1,17 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
+import {Text, View} from '@tarojs/components'
+import Taro, {useLoad} from '@tarojs/taro'
 import './index.less'
 
 export default function Detail() {
 
-  useLoad(() => {
-    console.log('Page loaded.')
+  useLoad((options) => {
+
+    console.log('Detail01=', options)
+    // @ts-ignore
+    const eventChannel = Taro.getCurrentInstance().page.getOpenerEventChannel()
+    eventChannel.on('acceptDataFromHomePage', function (data) {
+      console.log(data)
+    })
   })
 
   return (
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -30,6 +30,18 @@ export default function Home() {
     })
   }
 
+  const goToDetail04 = () => {
+
+    Taro.navigateTo({
+      url: PageConstants.getNavigateUrl(PageConstants.PAGE_DETAIL) + '?name=liujin&id=100',
+      success: function (res) {
+        res.eventChannel.emit('acceptDataFromHomePage', {
+          data: '我是从home页面传递detail04的数据',
+        })
+      }
+    })
+  }
+
   return (
     <View className='home'>
       <View>1. 页面跳转(组件)</View>
@@ -55,6 +67,9 @@ export default function Home() {
 
       <View>6. 页面切换API(组件)</View>
       <Button onClick={goToSwitch03}>goToSwitch03</Button>
+
+      <View>7. 页面事件发送</View>
+      <Button onClick={goToDetail04}>页面事件发送</Button>
 
     </View>
   )
