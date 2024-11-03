531ec64e6a8c99546a77060c3d579a41c6d3c942
wuyiccc <3406324191@qq.com>
2024-11-03 09:27:50 +0800

    feat: zustand使用

--- src/pages/zustand/index.tsx
+++ src/pages/zustand/index.tsx
@@ -1,18 +1,18 @@
 import {Text, View} from '@tarojs/components';
-import StatusDB from "@/infrastructure/status/StatusDB";
-import ZQButton from "@/component/zq-button";
+import StatusDB from '@/infrastructure/status/StatusDB';
+import ZQButton from '@/component/zq-button';
 
 export default function Zustand() {
 
 
-  const search = StatusDB.db((state) => state.search)
-  const setSearch = StatusDB.db((state) => state.setSearch)
+  const search = StatusDB.db((state) => state.search);
+  const setSearch = StatusDB.db((state) => state.setSearch);
 
 
   const onClick = () => {
 
-    setSearch(Date.now().toString())
-  }
+    setSearch(Date.now().toString());
+  };
 
   return (
     <View className='zustand'>
